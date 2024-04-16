fecc0eba555f9f922c9bcdb31300019f4934c30d
Mohammed Hattab <mohammedhattab97@gmail.com>
2024-04-15 20:31:39 -0400

    changed c2 ui

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,12 +2,13 @@ export const environment = {
   production: true,
   // rooturl : 'https://c2-server.mtattab.com',
   // wssurl: 'wss://c2-server.mtattab.com/reverseShellManager',
-  cSharpAPI: 'https://file7exfilterate.azurewebsites.net',
+  // rooturl : 'https://c2-server-tvan66p23a-uc.a.run.app',
+  // cSharpAPI : 'http://localhost:7109',
 
-  rooturl : 'https://c2-server-tvan66p23a-uc.a.run.app',
+  cSharpAPI: 'https://file7exfilterate.azurewebsites.net',
+  rooturl : 'https://command-and-control-de293e318d06.herokuapp.com',
   wssurl: 'wss://c2-server-tvan66p23a-uc.a.run.app/reverseShellManager',
   emailSpammerPython: 'https://email-spammer-tvan66p23a-uc.a.run.app',
-  // cSharpAPI : 'http://localhost:7109',
 
 
 
